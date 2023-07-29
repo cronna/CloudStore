1d407719c9f42fe09ca388977c8c6c855195441d
towel17 <towel1703@inbox.ru>
2023-07-29 18:46:16 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Главная</title>
+        <link rel="stylesheet" href="./assets/style/style.css">
+    </head>
+    <body>
+        <?php require './moduls/header.php' ?>
+        <div class="container">
+            <div class="item">
+                <img src="" alt="">
+                <h4 class="title_item">Название</h4>
+                <div class="rating">4.5⭐</div>
+                <span class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur cum dolorum nesciunt tempora temporibus.</span>
+                <a href="#">Купить</a>
+            </div>
+        </div>
+    </body>
+
+</html>
---
+++ moduls/header.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<header>
+    <a href="./">CLOUD</a>
+    <form action="" method="get">
+        <input type="text" placeholder="поиск">
+        <input type="submit" value='Найти'>
+    </form>
+    <a href="#">П</a>
+</header>
